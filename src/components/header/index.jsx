7b5d594ffa25ad92fa8e680be94c90566cf2a763
lolokos86 <lolokos1986@gmail.com>
2023-02-09 16:04:36 +0200

    new file:   src/components/header/constants.js 	modified:   src/components/header/index.jsx 	modified:   src/components/nav-item/index.jsx 	new file:   src/components/nav-menu/index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,12 +4,13 @@ import { NavItem } from "../nav-item";
 
 export const Header = () => {
   return (
-    <header className="flex">
+    <header className="flex ">
       <LogoIcon />
-      <nav>
-        <NavItem />
-        <NavItem />
-        <NavItem />
+      <nav className="flex ml-10 items-center">
+        <NavItem text="Feature" />
+        <NavItem text="Company" />
+        <NavItem text="Careers" />
+        <NavItem text="About" />
       </nav>
     </header>
   );
--- src/components/nav-item/index.jsx
+++ src/components/nav-item/index.jsx
@@ -1,12 +1,20 @@
-import React from "react";
-import {ReactComponents as ArrowDownIcon } from '../../images/ArrowDownIcon'
+import React, { useState } from "react";
+import { ReactComponent as ArrowDownIcon } from "../../images/icon-arrow-down.svg";
 
-export const NavItem = ({ text = "afafafsd" }) => {
+export const NavItem = ({ text, children }) => {
+    const [selected, setSelected] = useState('')
+
   return (
     <div className="relative">
-      <div className="flex space-x-2 cursor-pointer">
-        <span className="text-medium-gray hover:text-almost-black">{text}</span>
+      <div className="flex space-x-2 cursor-pointer items-center">
+        <span 
+        onClick={() => setSelected(text)}
+        className="text-medium-gray hover:text-almost-black">
+            {text}
+            </span>
+        <ArrowDownIcon />
       </div>
+      {children}
     </div>
   );
 };
