2c8f391c0d092dc378d96be58e0512b95ff8364d
lolokos86 <lolokos1986@gmail.com>
2023-02-09 17:15:20 +0200

    modified:   src/components/nav-item/index.jsx 	modified:   src/components/nav-menu/index.jsx

--- src/components/nav-item/index.jsx
+++ src/components/nav-item/index.jsx
@@ -5,7 +5,7 @@ export const NavItem = ({ text, children }) => {
   const [selected, setSelected] = useState("");
 
   return (
-    <div className="relative">
+    <div className="elative">
       <div className="flex space-x-2 cursor-pointer items-center">
         <span
           className="text-medium-gray hover:text-almost-black"
--- src/components/nav-menu/index.jsx
+++ src/components/nav-menu/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const NavMenu = ({ items = [] }) => {
   return (
-    <div className="flex flex-col px-4 py-2 bg-almost-white drop-shadow">
+    <div className="flex flex-col px-4 py-2 bg-almost-white drop-shadow rounded-lg absolute">
       {items.map(({text}) => (
         <span>{text}</span>
       ))}
