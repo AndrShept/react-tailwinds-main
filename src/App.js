580b82e540eb68359e74dba6f3e87c3fe96ddf52
lolokos86 <lolokos1986@gmail.com>
2023-02-09 12:08:29 +0200

    modified:   src/App.js 	new file:   src/components/wrapper/index.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import { Wrapper } from "./components/wrapper";
 
 function App() {
   return (
     <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Wrapper />
     </div>
   );
 }
