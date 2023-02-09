7b64dcdd34fa7503dbbd7cc3919f210e91ee18d2
lolokos86 <lolokos1986@gmail.com>
2023-02-09 20:56:49 +0200

    new file:   src/components/menu-item/index.jsx 	modified:   src/components/nav-menu/index.jsx

---
+++ src/components/menu-item/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const MenuItem = ({text = '', icon}) => {
+  return (
+    <div className='flex items-center space-x-2 w-full'>
+        {icon}
+        <span className='text-medium-gray hover:text-almost-black cursor-pointer'>{text}</span>
+    </div>
+  )
+}
