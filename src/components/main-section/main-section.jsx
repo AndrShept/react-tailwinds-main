71e54a4d6576a73e14a374d1211e80eec5a70cec
lolokos86 <lolokos1986@gmail.com>
2023-02-10 17:05:02 +0200

    modified:   src/components/main-section/main-section.jsx

--- src/components/main-section/main-section.jsx
+++ src/components/main-section/main-section.jsx
@@ -9,21 +9,21 @@ import { Button } from '../button';
 export const MainSection = () => {
   return (
     <section className='w-full flex mt-6 h-screen justify-between  '>
-      <div className=' relative text-left mt-12 '>
-        <h1 className='text-8xl font-medium mt-6 whitespace-pre-line'>{`Make\n remote work`}</h1>
-        <p className='text-medium-gray text-lg my-12 whitespace-pre-line '>
+      <div className=' relative text-left mt-12 bg-violet-100 p-3 rounded-md drop-shadow-md mb-4'>
+        <h1 className='text-8xl font-medium mt-6 whitespace-pre-line drop-shadow-lg'>{`Make\n remote work`}</h1>
+        <p className='text-medium-gray text-lg my-12 whitespace-pre-line  '>
           {`Lorem ipsum dolor, sit amet consectetur adipisicing elit.\n Amet ullam dolorum animi atque vitae exercitationem\n quibusdam maiores adipisci qui harum.`}
         </p>
         <Button isFilled={true}>Learn More</Button>
 
         <div className='flex justify-around absolute  w-full bottom-1'>
           <Databiz />
-          <Audiophile />
+          <Audiophile fill='red'/>
           <Meet />
           <Maker />
         </div>
       </div>
-      <div className='flex w-2/4 m-10 ' >
+      <div className='flex w-2/4 m-10 drop-shadow-lg ' >
         <img src={MenImage} alt="man" />
       </div>
     </section>
